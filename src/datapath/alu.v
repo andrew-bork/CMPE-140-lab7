dc4db2164a56f4f39229579cfcaba582c756435b
Andrew Lin <bork.andrew.ryan@gmail.com>
2025-10-21 11:30:50 -0700

    finish multiple

--- src/datapath/alu.v
+++ src/datapath/alu.v
@@ -1,20 +1,35 @@
 module alu (
-        input  wire [2:0]  op,
+        input  wire [3:0]  op,
         input  wire [31:0] a,
         input  wire [31:0] b,
+        input  wire [4:0]  shmt,
+        input  wire clk,
         output wire        zero,
         output reg  [31:0] y
     );
 
+    reg [63:0] multiplier_register;
+
     assign zero = (y == 0);
+
+    always @ (posedge clk) begin
+        if(op == 4'b1100) begin
+            multiplier_register = a * b;
+        end
+    end
 
     always @ (op, a, b) begin
         case (op)
-            3'b000: y = a & b;
-            3'b001: y = a | b;
-            3'b010: y = a + b;
-            3'b110: y = a - b;
-            3'b111: y = (a < b) ? 1 : 0;
+            4'b0000: y = a & b;
+            4'b0001: y = a | b;
+            4'b0010: y = a + b;
+            4'b0110: y = a - b;
+            4'b0111: y = (a < b) ? 1 : 0;
+
+            4'b1010: y = multiplier_register[63:32];
+            4'b1011: y = multiplier_register[31:0];
+
+            default: y = 32'hXXXX_XXXX;
         endcase
     end
 
--- src/datapath/datapath.v
+++ src/datapath/datapath.v
@@ -7,7 +7,7 @@ module datapath (
         input  wire        we_reg,
         input  wire        alu_src,
         input  wire        dm2reg,
-        input  wire [2:0]  alu_ctrl,
+        input  wire [3:0]  alu_ctrl,
         input  wire [4:0]  ra3,
         input  wire [31:0] instr,
         input  wire [31:0] rd_dm,
